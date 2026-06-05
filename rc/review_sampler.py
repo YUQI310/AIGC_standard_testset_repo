c221f2bb86e127f12af5f001c852c0b80a2abf32
YUQI310 <zyq310850@gmail.com>
2026-06-05 16:20:59 +0100

    Delete rc directory

--- rc/review_sampler.py
+++
@@ -1,8 +0,0 @@
-"""
-Review sampler placeholder.
-
-Expected responsibilities:
-- generate review sample lists
-- support label-stratified sampling
-- export review-ready records for manual checking
-"""
